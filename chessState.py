3f3bd684550c81a552178f33371e30e50e175564
Lumijek <lumijekpr@gmail.com>
2022-02-06 00:14:13 -0800

    initial commit

---
+++ chessState.py
@@ -0,0 +1,45 @@
+import pygame
+class chessEngine:
+	def __init__(self):
+		self.board = [["e" for j in range(8)] for i in range(8)]
+		self.pieces = dict()
+
+	def load_images(self):
+		self.pieces["R"] = pygame.image.load("pieces/Chess_rlt60.png").convert_alpha()
+		self.pieces["r"] = pygame.image.load("pieces/Chess_rdt60.png").convert_alpha()
+		self.pieces["B"] = pygame.image.load("pieces/Chess_blt60.png").convert_alpha()
+		self.pieces["b"] = pygame.image.load("pieces/Chess_bdt60.png").convert_alpha()
+		self.pieces["R"] = pygame.image.load("pieces/Chess_rlt60.png").convert_alpha()
+		self.pieces["r"] = pygame.image.load("pieces/Chess_rdt60.png").convert_alpha()
+		self.pieces["N"] = pygame.image.load("pieces/Chess_nlt60.png").convert_alpha()
+		self.pieces["n"] = pygame.image.load("pieces/Chess_ndt60.png").convert_alpha()
+		self.pieces["P"] = pygame.image.load("pieces/Chess_plt60.png").convert_alpha()
+		self.pieces["p"] = pygame.image.load("pieces/Chess_pdt60.png").convert_alpha()
+		self.pieces["K"] = pygame.image.load("pieces/Chess_klt60.png").convert_alpha()
+		self.pieces["k"] = pygame.image.load("pieces/Chess_kdt60.png").convert_alpha()
+		self.pieces["Q"] = pygame.image.load("pieces/Chess_qlt60.png").convert_alpha()
+		self.pieces["q"] = pygame.image.load("pieces/Chess_qdt60.png").convert_alpha()
+
+	def get_piece(self, piece):
+		return self.pieces[piece]
+
+	def create_board(self, fen):		
+		fen = fen.split("/")
+		for i in range(len(self.board)):
+			row = fen[i]
+			row_length = len(row)
+			j = 0
+			while j < row_length:
+				if row[j].isdigit():
+					j += int(row[j]) - 1
+					continue
+				else:
+					self.board[i][j] = row[j]
+					j += 1
+
+	def get_board(self):
+		return self.board
+
+	def get_piece_from_position(self, position):
+		return self.board[position[0]][position[1]]
+
---
+++ gui.py
@@ -0,0 +1,107 @@
+import pygame
+import sys
+import chessState
+
+from pygame.locals import *
+
+clock = pygame.time.Clock()
+
+pygame.init()
+
+WIDTH = 512
+
+HEIGHT = 512
+
+WINDOW_SIZE = (WIDTH, HEIGHT)
+
+board = pygame.display.set_mode(WINDOW_SIZE)
+
+INITIAL_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+engine = chessState.chessEngine()
+engine.load_images()
+engine.create_board(INITIAL_FEN)
+
+font = pygame.font.SysFont('Arial', 11)
+
+pygame.display.set_caption("Chess")
+
+GAME_COLORS = ((150, 200, 100), (200, 205, 200))
+
+GAME_COLORS_MAP = (GAME_COLORS[1], GAME_COLORS[0])
+
+HIGHLIGHT_COLORS = [(140, 255, 90), (255, 255, 255)]
+
+DIM = 8
+
+SQUARE_SIZE = WIDTH // DIM
+
+GAME_FPS = 15
+
+
+def draw_chess_board(board):
+    letter_position = 'abcdefgh'
+    number_position = [str(i) for i in range(1, 9)]
+
+    for i in range(DIM):
+        for j in range(DIM):
+            pygame.draw.rect(board, GAME_COLORS[(i + j) % 2], pygame.Rect(
+                i * SQUARE_SIZE, j * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+    for i in range(8):
+        number_color = GAME_COLORS_MAP[i % 2]
+        letter_color = GAME_COLORS[i % 2]
+
+        number = font.render(number_position[i], True, number_color)
+        board.blit(number, (3, i * SQUARE_SIZE + 3))
+
+        letter = font.render(letter_position[i], True, letter_color)
+        board.blit(letter, ((i + 1) * SQUARE_SIZE - 12, 8 * SQUARE_SIZE - 15))
+
+def render_pieces(board):
+    surface = engine.get_board()
+    for i in range(len(surface)):
+        for j in range(len(surface)):
+            if surface[i][j] != "e":
+                board.blit(engine.get_piece(surface[i][j]), (j * SQUARE_SIZE, i * SQUARE_SIZE))
+
+def piece_click(board, coordinates):
+    index = (coordinates[1] // SQUARE_SIZE, coordinates[0] // SQUARE_SIZE)
+    piece = engine.get_piece_from_position(index)
+    return index, piece
+
+
+def highlight_piece(board, index):
+        pygame.draw.rect(board, HIGHLIGHT_COLORS[(index[0] + index[1]) % 2], 
+        pygame.Rect(index[1] * SQUARE_SIZE, index[0] * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+
+def main():
+
+    index = None
+    clicked = False
+    previous_index = None
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                index, piece = piece_click(board, pos)
+                if previous_index != index:
+                    clicked = True
+                    previous_index = index
+                else:
+                    previous_index = None
+                    clicked = False
+
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        draw_chess_board(board)
+        if clicked == True:
+            highlight_piece(board, index)
+        render_pieces(board)
+        pygame.display.update()
+        clock.tick(GAME_FPS)
+
+if __name__ == "__main__":
+    main()
